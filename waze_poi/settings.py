d4bd1f4e9d55ceb107d897a1878fdfaa8bf7e2c5
Eder Xavier Rojas <edxavier05@gmail.com>
2017-08-18 21:39:17 -0600

    Change allowed hosts

--- waze_poi/settings.py
+++ waze_poi/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'wb*2ncku1&$r9iv6xet)cap=0*#)h!2vze&x5okzx)-1!@5p&^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -103,7 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-ni'
 
 TIME_ZONE = 'UTC'
 
