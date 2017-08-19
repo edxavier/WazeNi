c0eeef7ec53976f8bc4f887fe5cef5562dbb5338
Eder Xavier Rojas <edxavier05@gmail.com>
2017-08-18 23:40:57 -0600

    set serve statics

--- waze_poi/urls.py
+++ waze_poi/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from waze_poi import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 ]
+
